40e43cfa63fbdbdb87d8610963bce3a14afaec9f
Wayne Haworth <waynehaworth@gmail.com>
2025-12-09 11:43:38 +0000

    add initial backend unit tests

---
+++ utils/list_helper.js
@@ -0,0 +1,21 @@
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+export default {
+  dummy,
+  totalLikes,
+};
+
+/*
+_id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+*/
